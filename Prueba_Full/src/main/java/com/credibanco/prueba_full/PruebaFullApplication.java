39dfa37551e4a3f9d1784d0b3c0d5128d418a4be
Santiago-Gon <eversantiago07@hotmail.com>
2025-01-04 10:21:24 -0500

    Feat: Proyecto inicial backend

---
+++ Prueba_Full/src/main/java/com/credibanco/prueba_full/PruebaFullApplication.java
@@ -0,0 +1,13 @@
+package com.credibanco.prueba_full;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaFullApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaFullApplication.class, args);
+    }
+
+}
